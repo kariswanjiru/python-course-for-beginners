5fab9d83e2b62dfcc0e0d3518d3cb31471099527
kariswanjiru <wanjiruestar@gmail.com>
2022-12-05 12:09:32 +0300

    created calculations functions.

---
+++ calculation.py
@@ -0,0 +1,14 @@
+def calculations():
+    def add(a,b):
+        c = a+b
+        print(c)
+        return c
+
+    def sub(a,b):
+        if a < b:
+            c = b - a
+            print(c)
+        else:
+            c = a - b
+            print(c)
+ 
